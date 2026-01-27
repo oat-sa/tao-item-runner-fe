6cac5c879017af6fc7939e43dbe7ba3dc4568dea
Bartłomiej Marszał <bartlomiejmarszal@gmail.com>
2026-01-27 17:34:25 +0100

    fix: Remove unintended manager.js change

--- src/assets/manager.js
+++ src/assets/manager.js
@@ -233,13 +233,6 @@ var assetManagerFactory = function assetManagerFactory(strategies, data, options
             return resolved;
         },
 
-        resolveTranscription: function resolveTranscription(url, metadataUri, resourceUri) {
-            const resolvedResourceUri = this.resolveBy('taomedia', resourceUri);
-            const params = urlUtil.parse(resolvedResourceUri).query.uri;
-            const encodedMetadataUri = encodeURIComponent(metadataUri);
-            return `${url}?${encodedMetadataUri}${params}`;
-        },
-
         /**
          * When the cache is used, it could be useful to clear the cache
          */
